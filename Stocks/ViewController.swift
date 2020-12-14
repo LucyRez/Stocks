7221f363971056d015d83842133ca2ec7ba49031
Liuda <lrezunik@edu.hse.ru>
2020-12-14 15:08:31 +0200

    Added few more features

--- Stocks/ViewController.swift
+++ Stocks/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var companySymbolLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var priceChangeLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -32,7 +33,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         self.requestQuoteUpdate()
     }
     
-    private let companies: [String:String] = ["Apple":"AAPL",
+    private var companies: [String:String] = ["Apple":"AAPL",
                                               "Microsoft":"MSFT",
                                               "Google":"GOOG",
                                               "Amazon":"AMZN",
@@ -46,15 +47,17 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         self.companyPickerView.dataSource = self
         self.companyPickerView.delegate = self
         
-        self.requestQuoteUpdate()
+        self.getNewList()
     }
     
     private func requestQuoteUpdate(){
+        self.companyPickerView.reloadComponent(0)
         self.activityIndicator.startAnimating()
         self.comanyNameLabel.text = "-"
         self.companySymbolLabel.text = "-"
         self.priceLabel.text = "-"
         self.priceChangeLabel.text = "-"
+        self.imageView.image = nil
         
         let selectedRow = self.companyPickerView.selectedRow(inComponent: 0)
         let selectedSymbol = Array(self.companies.values)[selectedRow]
@@ -63,6 +66,8 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     private func requestQuote(for symbol:String){
         let url = URL(string: "https://cloud.iexapis.com/v1/stock/\(symbol)/quote?token=pk_25e21941845c4f51a74fe798445e666c")!
+        let imageUrl = URL(string: "https://cloud.iexapis.com/v1/stock/\(symbol)/logo?token=pk_25e21941845c4f51a74fe798445e666c")!
+        
         let dataTask = URLSession.shared.dataTask(with: url){
             data, response, error in guard
                 error == nil,
@@ -74,7 +79,20 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
             }
         self.parseQuote(data:data)
         }
+        
+        let imageTask = URLSession.shared.dataTask(with: imageUrl){
+            data, response, error in guard
+                error == nil,
+                (response as? HTTPURLResponse)?.statusCode == 200,
+                let data = data
+            else{
+                self.createAlert(withTitle: "Network error", andMessage: "Pronblems with network")
+                return
+            }
+        self.parseImageQuote(data: data)
+        }
         dataTask.resume()
+        imageTask.resume()
     }
     
     
@@ -102,8 +120,89 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
             self.createAlert(withTitle: "JSON parsing error", andMessage: "JSON parsing error: " + error.localizedDescription)
         }
     }
+    
+    private func parseImageQuote(data:Data){
+        do{
+            let jsonObject = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            guard
+            let json = jsonObject as? [String:Any],
+            let image = json["url"] as? String
+            else {
+            return
+            }
+            let url = URL(string: image)!
+            DispatchQueue.main.async {
+            self.downloadImage(from: url)
+            }
+        }catch{
+            self.createAlert(withTitle: "JSON parsing error", andMessage: "JSON parsing error: " + error.localizedDescription)
+        }
+    }
+    
+    private func parseListQuote(data:Data){
+        do{
+            let jsonObject = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            guard
+            let json = jsonObject as? [Any]
+            else {
+            return
+            }
+            var updated = [String:String]()
+            for group in json{
+                guard
+                    let company = group as? [String:Any],
+                    let symbol = company["symbol"] as? String,
+                    let companyName = company["companyName"] as? String
+                else{
+                  return
+                }
+                updated[companyName] = symbol
+                DispatchQueue.main.async {
+                    self.updateList(companies: updated)
+                }
+            }
+        }catch{
+            self.createAlert(withTitle: "JSON parsing error", andMessage: "JSON parsing error: " + error.localizedDescription)
+        }
+    }
+    
+    func getImage(from url: URL, completion: @escaping (Data?, URLResponse?, Error?)->()){
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+    
+    func downloadImage(from url: URL){
+        getImage(from: url) { data, response, error in
+            guard let data = data, error == nil else {return}
+            DispatchQueue.main.async {
+                [weak self] in self?.imageView.image = UIImage(data: data)
+            }
+        }
+    }
+    
+    private func getNewList(){
+        let listUrl = URL(string: "https://cloud.iexapis.com/v1/stock/market/list/mostactive?&token=pk_25e21941845c4f51a74fe798445e666c")!
+        
+        let listTask = URLSession.shared.dataTask(with: listUrl){
+            data, response, error in guard
+                error == nil,
+                (response as? HTTPURLResponse)?.statusCode == 200,
+                let data = data
+            else{
+                self.createAlert(withTitle: "Network error", andMessage: "problems with network")
+                return
+            }
+        self.parseListQuote(data:data)
+        }
+        listTask.resume()
+        
+    }
+    
+    func updateList(companies: [String:String]){
+        self.companies = companies
+        self.requestQuoteUpdate()
+    }
    
-    private func createAlert(withTitle title: String, andMessage message: String) {
+    func createAlert(withTitle title: String, andMessage message: String) {
            DispatchQueue.main.async {
                let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
                
